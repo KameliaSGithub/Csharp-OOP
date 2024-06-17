708fbfe1a0e89a75cbaebe2093d54bb4b9d3e386
Kamelia <143409916+KameliaSGithub@users.noreply.github.com>
2024-06-17 23:27:43 +0300

    Add files via upload

---
+++ C#oop/randomList/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace randomList
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            RoboticRandomList roboList = new RoboticRandomList();
+            roboList.Add("servo");
+            roboList.Add("sensor");
+            roboList.Add("actuator");
+            roboList.Add("controller");
+
+            Console.WriteLine("Removed part: " + roboList.ExtractRandomPart());
+            Console.WriteLine("Remaining robotic parts:");
+            foreach (var part in roboList)
+            {
+                Console.WriteLine(part);
+            }
+        }
+    }
+
+    public class RoboticRandomList : List<string>
+    {
+        private Random randomizer = new Random();
+
+        public string ExtractRandomPart()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("Robotic part list is empty.");
+            }
+
+            int index = randomizer.Next(0, this.Count);
+            string selectedPart = this[index];
+            this.RemoveAt(index);
+            return selectedPart;
+        }
+    }
+}
